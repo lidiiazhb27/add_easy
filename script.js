4b609f77f075eadbbce8b877eff05b622ac89515
Lidiia Zhbadynska <76854114+lidiiazhb27@users.noreply.github.com>
2021-12-24 15:28:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function change(){
+    var giftChange = document.getElementById('giftChange');
+    giftChange.src = "gift1.jpg";
+}
+function change2(){
+    var giftChange = document.getElementById('giftChange2');
+    giftChange.src = "gift2.jpg";
+}
+function change3(){
+    var giftChange = document.getElementById('giftChange3');
+    giftChange.src = "gift3.jpg";
+}
+function change4(){
+    var giftChange = document.getElementById('giftChange4');
+    giftChange.src = "gift4.jpg";
+}
+function change5(){
+    var giftChange = document.getElementById('giftChange5');
+    giftChange.src = "gift5.jpg";
+}
